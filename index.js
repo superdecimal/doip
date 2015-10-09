755486588e2b25c428ea9bb2bf5949dcaa25f7a6
Paris Goudas <paris@gordius.gr>
2015-10-10 02:47:04 +0300

    Minor bugfix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var doInst = new digitalOcean(config.apiKey);
 
 findIp().then(function (ip) {
   doInst.getDomainRecord(config.domain, config.record).then(function (domainRecord) {
-    return doInst.updateDomainRecord(config.domain, config.record, domainRecord.id, ip);§
+    return doInst.updateDomainRecord(config.domain, config.record, domainRecord.id, ip);
   })
   .then(function (data) {
     console.log(new Date() + " IP: " + data.domain_record.data) ;
